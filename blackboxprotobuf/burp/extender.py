310f20a4fe4c7539cb528504640276931d2a7dc0
Ryan Winkelmaier <ryan.winkelmaier@nccgroup.com>
2021-10-04 20:44:44 -0600

    Updated protobuf library version for burp

--- blackboxprotobuf/burp/extender.py
+++ blackboxprotobuf/burp/extender.py
@@ -10,8 +10,23 @@ import burp
 # Add correct directory to sys.path
 _BASE_DIR = os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe())) + '../../../')
 sys.path.insert(0, _BASE_DIR)
-sys.path.insert(0, _BASE_DIR + '/burp-deps/protobuf/python/')
 sys.path.insert(0, _BASE_DIR + '/burp-deps/six/')
+
+# Hack to fix loading protobuf libraries within Jython. See https://github.com/protocolbuffers/protobuf/issues/7776
+def fix_protobuf():
+    import six
+    u = six.u
+    def new_u(s):
+        if s == r'[\ud800-\udfff]':
+            # Don't match anything
+            return '$^'
+        else:
+            return u(s)
+    six.u = new_u
+fix_protobuf()
+
+
+sys.path.insert(0, _BASE_DIR + '/burp-deps/protobuf/python/')
 
 from blackboxprotobuf.burp import editor, typedef_tab
 
